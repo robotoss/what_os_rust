891255539ab6a4a6443f89fcbe1f8b8fa786841b
Yeftifeyev Konstantin <zoxo@outlook.com>
2022-02-27 11:19:16 +0200

    add x86 build

---
+++ rust/target/i686-linux-android/release/deps/greeter.d
@@ -0,0 +1,7 @@
+C:\Users\35799\OneDrive\Documents\what_os_rust\rust\target\i686-linux-android\release\deps\libgreeter.a: src\lib.rs
+
+C:\Users\35799\OneDrive\Documents\what_os_rust\rust\target\i686-linux-android\release\deps\libgreeter.so: src\lib.rs
+
+C:\Users\35799\OneDrive\Documents\what_os_rust\rust\target\i686-linux-android\release\deps\greeter.d: src\lib.rs
+
+src\lib.rs:
---
+++ rust/target/i686-linux-android/release/libgreeter.d
@@ -0,0 +1,1 @@
+C:\Users\35799\OneDrive\Documents\what_os_rust\rust\target\i686-linux-android\release\libgreeter.a: C:\Users\35799\OneDrive\Documents\what_os_rust\rust\src\lib.rs
